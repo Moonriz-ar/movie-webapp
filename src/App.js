92c9c9c429d189a271d3c322aebee4e3dc2379f9
Andrea Lin <andre93_lin@hotmail.com>
2022-04-13 15:05:50 -0300

    fixed bug, deleted import of test.js component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./componentes/Login";
 import Listado from "./componentes/Listado";
-import Test from "./componentes/Test";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} end />
         <Route path="/listado" element={<Listado />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </>
   );
