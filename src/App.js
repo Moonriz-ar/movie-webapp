e33bf5e8fad814f23a21084d74dd54f6ac83f238
Andrea Lin <andre93_lin@hotmail.com>
2022-04-15 08:27:05 -0300

    Index.js - refactored to react 18 create root instead of ReactDOM.render

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Listado from "./componentes/Listado";
 
 function App() {
   return (
-    <>
+    <div id="app">
       <Routes>
         <Route path="/" element={<Login />} end />
         <Route path="/listado" element={<Listado />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
