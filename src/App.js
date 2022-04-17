e0d2ce08e57d8c65d236e221031d1f613f3d1573
Andrea Lin <andre93.lin@gmail.com>
2022-04-17 10:47:39 -0300

    set up tailwind framework

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Listado from "./componentes/Listado";
 function App() {
   return (
     <div id="app">
+      <h1 className="underline">Hello world!</h1>
       <Routes>
         <Route path="/" element={<Login />} end />
         <Route path="/listado" element={<Listado />} />
