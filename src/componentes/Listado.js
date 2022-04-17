9e45b376c339eade3df038fe4c96c216c1c02b65
Andrea Lin <andre93.lin@gmail.com>
2022-04-17 13:34:25 -0300

    set up env file for movie api base url and key. set up api call to get movies list in listado.js component

--- src/componentes/Listado.js
+++ src/componentes/Listado.js
@@ -1,8 +1,29 @@
 import { Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import Card from "./Card";
 
 function Listado() {
   const token = localStorage.getItem("token");
+
+  const [moviesList, setMoviesList] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch(
+        `${process.env.REACT_APP_MOVIE_BASE_URL}${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const apiData = data.results;
+          setMoviesList(apiData);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [setMoviesList]);
 
   return (
     <>
